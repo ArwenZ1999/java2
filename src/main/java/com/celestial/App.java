5cfbc975242951e9cf90cff933173d1d9cc2f53a
yawenGit <arwen.zhang@outlook.com>
2022-10-20 13:06:08 +0100

    explandible array

--- src/main/java/com/celestial/App.java
+++ src/main/java/com/celestial/App.java
@@ -1,5 +1,6 @@
 package com.celestial;
 
+import java.util.Arrays;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
@@ -11,22 +12,63 @@ public class App
 {
     public static void main( String[] args )
     {
-        String lineRead;
+	
+    	
+    	System.out.print("Enter the price, enter quit to exit:");
+    	String lineRead;
         Scanner sc = new Scanner(System.in);
-        
+        int growBy=1;
+        String[] prices=new String[growBy];//create an array; static array
         try
-        {
-            while( prompt() && (lineRead = sc.next()) != null )
+        {        
+            for (int x =0;(x<prices.length && prompt() && ( lineRead=sc.next())!=null);x++)
             {
-                System.out.println(lineRead);
+            	if(lineRead.equalsIgnoreCase("quit"))
+            		break;
+            	prices[x]=lineRead;
+            	System.out.println(lineRead);
+            	if(x==prices.length-1)        	
+            		//the purpose of copyof is 
+            		prices= Arrays.copyOf(prices, prices.length+growBy);         	
             }
         }catch( NoSuchElementException e )
         {}
+        //error1: cannot convert null to double 
+       // double[] doubleValues = Arrays.stream(prices)
+            //    .mapToDouble(Double::parseDouble)
+           //     .toArray();
+    	
+    	String[] SlicedPrices=new String[prices.length-1];
+
+      ///////get rid of null   	
+
+        for(int z=0;z<prices.length-1;z++)
+    	{
+    		SlicedPrices[z]=prices[z];
+    	}  
+        /////Convert//////
+        double[] doubleValues = Arrays.stream(SlicedPrices)
+                    .mapToDouble(Double::parseDouble)
+                    .toArray();
+        //////sort/////
+        Arrays.sort(doubleValues);
+        //////test///////
+        for (double doubleValue : doubleValues)
+        {
+        	 System.out.println(doubleValue);
+        }
+    }
+    
+    double create_vat(double cost, double vat)
+    {
+    	return cost*vat;
     }
     
     static private boolean prompt()
     {
-        System.out.print("Press a key:");
+        System.out.print("enter next price:");
         return true;
     }
+
+	   
 }
